b8a0e6ee6be5032964b62034c5da3e204e5a22a7
Marcelo Gonzalez <marcegon13@gmail.com>
2023-05-30 22:21:16 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function calcularPorcentaje(){
+  let numero = (document.getElementById("numero").value);
+  let porcentaje = (document.getElementById("porcentaje").value);
+
+  console.log (numero * porcentaje / 100);
+
+}
